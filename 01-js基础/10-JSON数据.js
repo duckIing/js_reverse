29667a3412a715da4d9c72f70dbc8104028d9da4
duckling <chenhuangjie1995@gmail.com>
2025-09-12 22:43:27 +0800

    docs(JSON数据): 添加注释并完善JSON示例代码的调试输出
    添加关于爬虫API返回JSON格式数据的注释
    完善示例代码的调试输出，增加类型检查和分隔线
    保持字符串转换的注释说明

--- 01-js基础/10-JSON数据.js
+++ 01-js基础/10-JSON数据.js
@@ -1,4 +1,5 @@
 /*
+在 爬虫中，api接口返回的数据一般是 json 格式。
 json是JavaScript Object Notation的首字母缩写，翻译过来就是JavaScript对象表示法，
 这里说的json就是类似于JavaScript对象的字符串，它同时是一种数据格式，
 目前这种数据格式比较流行，逐渐替换掉了传统的xml数据格式。
@@ -20,19 +21,23 @@ var json_data = {
     }
 }
 console.log(json_data);
-
+console.log(typeof json_data);
+console.log('--------------------------------------------------1')
 
 // json数据格式转为JavaScript对象
-var json = '{"name": "admin", "age": 123}'
+var json = '{"name": "admin", "age": 123}'  // 字符串。
+console.log(json);
+console.log(typeof json)
+console.log('--------------------------------------------------2')
+
 var json_obj = JSON.parse(json)  // 将字符串转为对象
-
+console.log(json_obj);
 console.log(typeof json_obj);
+console.log('--------------------------------------------------3')
 
-console.log(json_obj)
 console.log(json_obj.name)  // 访问对象属性, 类似python中的字典
-
-console.log('-'.repeat(30))
+console.log('--------------------------------------------------4')
 
 var json_str = JSON.stringify(json_obj) // 将对象转为字符串
 console.log(typeof json_str)
-console.log(json_str)  // 转为字符串后没有多余的空格(后期爬虫会遇到这个问题)
+console.log(json_str)  // 转为字符串后没有多余的空格 ( 后期爬虫会遇到这个问题 )
