d9a89e22d0bc4b770645ec0b07d3aa1905841f3b
duckling <chenhuangjie1995@gmail.com>
2025-09-12 21:42:56 +0800

    docs(定时器): 更新定时器相关注释和示例代码
    添加关于爬虫中定时器重要性的说明
    完善定时器使用示例，包括执行顺序观察和错误写法示例
    补充定时器清除的多种方法说明

--- 01-js基础/08-定时器.js
+++ 01-js基础/08-定时器.js
@@ -1,6 +1,8 @@
 /*
 定时器就是在一段特定的时间后执行某段程序代码。
 
+在爬虫中，定时器非常重要，和无线debuger有关系。
+
 定时器的使用
   JavaScript定时器有两种创建方式:
     1. setTimeout(func[, delay, params_1, params_2, ...]) ：以指定的时间间隔（以毫秒计）调用一次函数的定时器
@@ -22,13 +24,19 @@
       ● clearInterval(timeoutID)：清除反复执行的定时器（setInterval函数）
 */
 
+// // 测试 setTimeout
 // function run_test1(){
 //     console.log("这是一个测试函数1");
 // }
-
+// console.log('run...1')
 // /* setTimeout: 延迟执行一次 */
-// setTimeout(run_test1, 1000);  // timeout: 单位是毫秒
+// setTimeout(run_test1, 2000);  // timeout: 单位是毫秒
 
+// // 观察顺序
+// console.log('run...2')
+
+
+// // 测试 setInterval
 // function run_test2(){
 //     console.log("这是一个测试函数2");
 // }
@@ -36,7 +44,12 @@
 // /* setInterval: 周期性执行 */
 // setInterval(run_test2, 1000);
 
-// // 1和2一起输出
+// // 观察顺序
+// console.log('run...3')
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////
 
 
 /*
@@ -45,30 +58,43 @@ JavaScript清除定时器分别是：
   ● clearTimeout(timeoutID)：清除只执行一次的定时器（setTimeout函数）
   ● clearInterval(timeoutID)：清除反复执行的定时器（setInterval函数）
 */
-function test() {
-  console.log("这是一个测试函数");
-};
 
-/* setTimeout: 延迟执行一次 */
-let timeoutID = setTimeout(test, 1000);
+// function test() {
+//   console.log("这是一个测试函数");
+// };
 
-/* 清除定时器 */
-clearTimeout(timeoutID);
+// /* setTimeout: 延迟执行一次 */
+// let timeoutID = setTimeout(test, 1000);
 
-/* setInterval: 周期性执行 */
-let intervalID = setInterval(test, 1000);
+// /* 清除定时器 */
+// clearTimeout(timeoutID);
 
-/* 清除定时器 */
-clearInterval(intervalID);
+// function test_func() {
+//   console.log("这是一个测试函数 setInterval");
+// };
 
+// /* setInterval: 周期性执行 */
+// let intervalID = setInterval(test_func, 1000);
 
+// /* 5.1秒后，清除定时器 intervalID  */
+// setTimeout(() => {
+//     clearInterval(intervalID);
+// }, 5100);
+
+// 下面是错误写法
+// setTimeout(clearInterval(intervalID), 5100); // 错误写法，会立即执行 clearInterval(intervalID)，传递的是 函数执行结果
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+// 结束定时器的另一种写法：方法置空
 function run_test(){
     console.log("这是一个测试函数");
 }
 
+// 将原有的 setInterval 方法指向为自定义的函数
 setInterval = function () {
-    // console.log(arguments[0]);
+    // console.log(arguments[0]); // 注意：arguments 是收集的参数
     arguments[0]();
 }
 
-setInterval(run_test)
+setInterval(run_test) // run_test 传入后，会被 arguments 收集起来
